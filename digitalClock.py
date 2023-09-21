6223771c591693a8e5140bfbc1df5908c325949f
OmarHSalim <Omar.Salim.eng@gmail.com>
2023-09-21 16:47:21 +1000

    Add Omar Name

--- digitalClock.py
+++ digitalClock.py
@@ -5,7 +5,7 @@ window.title("Digital CLock")
 window.geometry("600x300")
 window.configure(bg="darkgreen")
 
-label = Label(window, text="Hello!", font=("Arial Black",78,"bold"), bg="darkgreen", fg="white")
+label = Label(window, text="OMAR!", font=("Arial Black",78,"bold"), bg="darkgreen", fg="white")
 label.pack(pady=100)
 
 window.mainloop() 
